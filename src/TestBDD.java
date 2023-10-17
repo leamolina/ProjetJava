8272a6879e2daa321b06d59f7fa29bcde521bcfc
leamolina <120037925+leamolina@users.noreply.github.com>
2023-10-17 14:44:31 +0200

    Tests sur l'interface

--- src/TestBDD.java
+++ src/TestBDD.java
@@ -1,8 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class TestBDD {
 
@@ -11,18 +7,24 @@ public class TestBDD {
     public static void main(String args[]){
 
         try {
-            String request = "select nom from test where id=1;";
+            String request = "select nom, prenom from test where nom=?;";
             String url = "jdbc:postgresql://localhost:5432/TestBDDJava";
             String username = "postgres";
             String password = "passwordDatabase";
 
             Connection con = DriverManager.getConnection(url, username, password);
 
-            Statement st = con.createStatement(); //ATTENTION : pas bien d'utiliser Statement, mieux vaut apprendre à utiliser un prepared statement (+ sûr si on a des interactions avec l'utilisateur et qu'on sait pas toujours ce qu'il va rentrer)
-            ResultSet result = st.executeQuery(request); //Renvoie un objet de type ResultSet : un data
-            result.next(); //Pck sinon ca pointe vers la premiere colonne (celle avec les numeros des lignes)
-            String name = result.getString(1);
-            System.out.println(name);
+            PreparedStatement preparedStatement = con.prepareStatement(request);
+            preparedStatement.setString(1, "Molina");
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            while (result.next()) {
+                String nom = result.getString("nom");
+                String prenom = result.getString("prenom");
+                System.out.println(nom + " " + prenom);
+            }
+
             con.close();
         }
         catch(SQLException e){
---
+++ src/TestInterface.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class TestInterface {
+    private JPanel Bonjour;
+    private JButton envoyerButton;
+    private JPasswordField identifiantPasswordField;
+    private JTextField préRemplissageTextField;
+    private JCheckBox enregistrerMesInformationsCheckBox;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
